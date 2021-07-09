e35408cbed9d32e74069e4bc73dfa87891975f69
Marika Shanahan <marikashanahan@ggmail.com>
2021-07-09 14:57:14 -0600

    Add acceptance criteria to the top of server

--- server.js
+++ server.js
@@ -1,3 +1,15 @@
+// ACCEPTANCE CRITERIA
+
+// You should provide your own project, not the example URL.
+
+// You can submit a form that includes a file upload.
+
+// The form file input field has the name attribute set to upfile.
+
+// When you submit a file, you receive the file name, type, and size in bytes within the JSON response.
+
+
+
 var express = require('express');
 var cors = require('cors');
 require('dotenv').config()
