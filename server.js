6c0850882eba47ae9a458f7240b03bc1a562f4cf
Marika Shanahan <marikashanahan@ggmail.com>
2021-07-09 15:43:10 -0600

    Use express json and urlencoder with req.body response on file upload

--- server.js
+++ server.js
@@ -2,28 +2,33 @@
 
 // You should provide your own project, not the example URL.
 
-// You can submit a form that includes a file upload.
+// You can submit a form that includes a file upload. ( /api/fileanalyse )
 
 // The form file input field has the name attribute set to upfile.
 
 // When you submit a file, you receive the file name, type, and size in bytes within the JSON response.
 
-
-
 var express = require('express');
 var cors = require('cors');
-require('dotenv').config()
+require('dotenv').config();
 
 var app = express();
 
 app.use(cors());
+
+app.use(express.json());
+
+app.use(express.urlencoded());
+
 app.use('/public', express.static(process.cwd() + '/public'));
 
 app.get('/', function (req, res) {
     res.sendFile(process.cwd() + '/views/index.html');
 });
 
-
+app.post('/api/fileanalyse', (req, res) => {
+  res.json(req.body)
+})
 
 
 const port = process.env.PORT || 3000;
